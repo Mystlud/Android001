8c83bb7ec8ff805381cde32c4e38108f9a96725e
Mystlud <bayurahmatr@gmail.com>
2022-10-26 21:46:15 +0700

    tugas-try1

--- app/src/main/java/com/example/baybay/HaiHaiActivity.kt
+++ app/src/main/java/com/example/baybay/HaiHaiActivity.kt
@@ -7,11 +7,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.PathNode
 import androidx.compose.ui.res.painterResource
@@ -20,18 +23,96 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class HaiHaiActivity : ComponentActivity(){
+    @Composable
+    fun view(){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.love_you_forever),
+                contentDescription = "Gambar",
+                alignment = Alignment.Center,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(30.dp, 30.dp)
+            )
+            Text(
+                text = "HaiHai Kroos desu yo",
+                fontSize = 10.sp,
+                modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 4.dp),
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "Doctor",
+                fontSize = 10.sp,
+                modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 4.dp),
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val warnahijau = 0x07f547
+        //val warnahijau = 0x07f547
+
         setContent(){
+            Column() {
+                Row() {
+                    Surface(
+                        //modifier = Modifier.aspectRatio(),
+                        color = Color.Red
+                    ){
+                        view()
+                    }
+                    Surface(
+                        //modifier = Modifier.fillMaxWidth(0.3f),
+                        color = Color.Green
+                    ){
+                        view()
+                    }
+                    Surface(
+                        //modifier = Modifier.fillMaxWidth(0.3f),
+                        color = Color.Yellow
+                    ){
+                        view()
+                    }
+                }
+
+                Row() {
+                    Surface(
+                        //modifier = Modifier.fillMaxWidth(),
+                        color = Color.Blue
+                    ){
+                        view()
+                    }
+                    Surface(
+                        //modifier = Modifier.fillMaxWidth(),
+                        color = Color.Magenta
+                    ){
+                        view()
+                    }
+                    Surface(
+                        //modifier = Modifier.fillMaxWidth(),
+                        color = Color.LightGray
+                    ){
+                        view()
+                    }
+
+                }
+            }
+        }
+
+/*        setContent(){
             Column(
                 modifier = Modifier
                     .background(Color.Green)
                     .fillMaxSize(),
-               horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-
                 Image(
                     painter = painterResource(id = R.drawable.love_you_forever),
                     contentDescription ="Gambar")
@@ -61,6 +142,6 @@ class HaiHaiActivity : ComponentActivity(){
                 }
             }
 
-        }
+        }*/
     }
 }
